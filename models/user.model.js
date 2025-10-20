3c8f4f6de624952160d00f91158c726f20ef6060
harshk0890 <kumarharsh54376@gmail.com>
2025-10-20 15:55:04 +0530

    Add files via upload

---
+++ models/user.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+     email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+     password:{
+        type:String,
+        required:true
+    },
+     assitantName:{
+        type:String
+    },
+     assitantImage:{
+        type:String
+    },
+    history:[
+        {type :String}
+    ]
+
+}, { timestamps:true})
+const User = mongoose.model("User",userSchema)
+export default User
